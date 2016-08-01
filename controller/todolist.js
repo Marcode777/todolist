a85371f97938d1dff1465fe763178abf56f44cf0
Marcode777 <marcosebello7@gmail.com>
2016-08-01 06:12:27 -0400

    modified controller/todolist.js, view/todolist.css, view/todolist.html, added spinning animation as on-click event to verify action has been made

--- controller/todolist.js
+++ controller/todolist.js
@@ -1,8 +1,20 @@
  
-TweenMax.from("h1", 2.5, {x:-600, rotation:360});
+TweenMax.from("h1", 2, {x:-600});
 
  // var Anim = TweenMax.to(".w", 1, { x:-100 , opacity:0 , ease:Power1.easeInOut ,repeat:-1 , paused:true });
 
+ var Anim = TweenMax.fromTo('.spin', 0.5, {
+  rotation: 360, 
+},
+{
+  rotation: -360,
+  // delay: 1,
+  // repeat: -1,
+  // repeatDelay: 1,
+  yoyo: true,
+  immediateRender:false,
+  paused:true
+});
 
 function TodoCtrl($scope){
   $scope.todos =[
@@ -11,7 +23,7 @@ function TodoCtrl($scope){
   {text:'3', done:false}
   ];
 
-  // $('.btn').click( function(){  Anim.play()  }) ;
+  $('.btn').click( function(){  Anim.play(0)  }) ; //the value 0 will help the animation from the beginning each time the button is clicked
 
   $scope.getTotalTodos = function(){
     return $scope.todos.length;
